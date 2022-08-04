bb8dd7b6cbb329cd5fc4681fafb8659efd10a50a
t-sin <shinichi.tanaka45@gmail.com>
2022-08-04 19:14:25 +0900

    Add some traits to syntax tree parts

--- src/lang/syntax_tree.rs
+++ src/lang/syntax_tree.rs
@@ -1,15 +1,19 @@
+#[derive(Debug, PartialEq)]
 pub enum Type {
     Float,
     String,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Name(pub String);
 
+#[derive(Debug, PartialEq)]
 pub struct Arg {
     pub name: Name,
     pub r#type: Type,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Op2 {
     Add,
     Sub,
@@ -17,24 +21,30 @@ pub enum Op2 {
     Div,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Symbol {
+    Var(Name),   // foo
+    State(Name), // $pos_x
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Expr {
     Float(f32),
+    String(String),
+    Symbol(Symbol),
     Op2(Op2, Box<Expr>, Box<Expr>),
 }
 
-pub enum AssignTarget {
-    VarName(Name),   // foo
-    StateName(Name), // $pos_x
-}
-
+#[derive(Debug, PartialEq)]
 pub enum Body {
-    LexicalAssign(AssignTarget, Expr),
+    LexicalAssign(Symbol, Expr),
     FnCall(Name, Vec<Expr>),
     If(Expr, Vec<Body>, Vec<Body>),
     Return(Expr),
 }
 
+#[derive(Debug, PartialEq)]
 pub enum SyntaxTree {
-    GlobalAssign(AssignTarget, Expr),
+    GlobalAssign(Symbol, Expr),
     Defun(Name, Vec<Arg>, Vec<Body>),
 }
