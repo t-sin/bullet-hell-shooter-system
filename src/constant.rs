5b346c403c023722dbe6df084a98a9df44dfdcf4
t-sin <shinichi.tanaka45@gmail.com>
2022-08-19 00:05:12 +0900

    Change window size

--- src/constant.rs
+++ src/constant.rs
@@ -1,10 +1,10 @@
-pub const WIDTH: f32 = 800.0;
-pub const HEIGHT: f32 = 600.0;
+pub const WIDTH: f32 = 1280.0;
+pub const HEIGHT: f32 = 960.0;
 
 pub const SHOOTER_OFFSET_X: f32 = 200.0;
-pub const SHOOTER_OFFSET_Y: f32 = 50.0;
-pub const SHOOTER_WIDTH: f32 = 400.0;
-pub const SHOOTER_HEIGHT: f32 = 500.0;
+pub const SHOOTER_OFFSET_Y: f32 = 60.0;
+pub const SHOOTER_WIDTH: f32 = 600.0;
+pub const SHOOTER_HEIGHT: f32 = 840.0;
 
 pub const SHOOTER_PLAYER_AREA_X1: f32 = 10.0;
 pub const SHOOTER_PLAYER_AREA_Y1: f32 = 10.0;
